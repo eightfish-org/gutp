6a2b974b8743e22cce35e8130e9d0150e25461a9
Daogang Tang <daogangtang@gmail.com>
2023-05-01 17:54:54 +0800

    add gutp-types.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- gutp-types/src/lib.rs
+++ gutp-types/src/lib.rs
@@ -1,14 +1,77 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, EightFishModel)]
+struct GutpUser {
+    id: String,
+    account: String,
+    nickname: String,
+    avatar: String,
+    role: u16,
+    status: u16,
+    signup_time: u64,
+    ext: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct GutpSubspace {
+    id: String,
+    title: String,
+    description: String,
+    banner: String,
+    owner: String,
+    profession: String,
+    appid: String,
+    private: bool,
+    status: u16,
+    weight: u16,
+    created_time: u64,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+struct GutpPost {
+    id: String,
+    title: String,
+    content: String,
+    author_id: String,
+    subspace_id: String,
+    extlink: String,
+    profession: String,
+    appid: String,
+    private: bool,
+    status: u16,
+    weight: u16,
+    created_time: u64,
+    updated_time: u64,
+}
+
+struct GutpComment {
+    id: String,
+    content: String,
+    author_id: String,
+    post_id: String,
+    parent_comment_id: String,
+    private: bool,
+    status: u16,
+    created_time: u64,
+}
+
+struct GutpTag {
+    id: String,
+    caption: String,
+    subspace_id: String,
+    creator_id: String,
+    weight: u16,
+    private: bool,
+    created_time: u64,
+}
+
+struct GutpModerator {
+    id: String,
+    user_id: String,
+    subspace_id: String,
+    subspace_moderator: bool,
+    tag_id: String,
+    permission_level: u16,
+}
+
+struct GutpPostTag {
+    id: String,
+    post_id: String,
+    tag_id: String,
 }
