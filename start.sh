9f99309f6256a5e569b14ec8ea7052b70bc9e9b3
Daogang Tang <daogangtang@gmail.com>
2024-08-22 17:32:00 -0700

    update manifest to spin v2.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

---
+++ start.sh
@@ -0,0 +1,1 @@
+SPIN_VARIABLE_PROTO_ID="gutp" spin up -f spin_gutp.toml -e REDIS_URL_ENV="redis://localhost:6379" -e DB_URL_ENV="host=localhost user=postgres password=postgres dbname=gutp sslmode=disable"
