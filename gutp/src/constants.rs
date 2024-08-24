422340419498cf1ea6dd510755ad35e9c540e43b
Daogang Tang <daogangtang@gmail.com>
2024-08-23 22:43:39 -0700

    unify the format of the env variables.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- gutp/src/constants.rs
+++ gutp/src/constants.rs
@@ -1,3 +1,3 @@
-pub const REDIS_URL_ENV: &str = "REDIS_URL";
-pub const DB_URL_ENV: &str = "DB_URL";
+pub const REDIS_URL_ENV: &str = "REDIS_URL_ENV";
+pub const DB_URL_ENV: &str = "DB_URL_ENV";
 pub const PAGESIZE: usize = 25;
